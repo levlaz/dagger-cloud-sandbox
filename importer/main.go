b149c2f2be3604afb4bf65bb9201dfa91206f02b
Lev Lazinskiy <lev@dagger.io>
2024-01-25 13:07:32 -0800

    show examples of all sdks

---
+++ importer/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+)
+
+type Importer struct {}
+
+// example usage: "dagger call container-echo --string-arg yo"
+func (m *Importer) ContainerEcho(stringArg string) *Container {
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+}
+
+// example usage: "dagger call grep-dir --directory-arg . --pattern GrepDir"
+func (m *Importer) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
+	return dag.Container().
+		From("alpine:latest").
+		WithMountedDirectory("/mnt", directoryArg).
+		WithWorkdir("/mnt").
+		WithExec([]string{"grep", "-R", pattern, "."}).
+		Stdout(ctx)
+}
+
